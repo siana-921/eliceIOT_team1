86fb7f904035f85b59428e6e9f38244bfd9943d1
김정연 <jy.keem2468@gmail.com>
2023-04-26 21:28:15 +0900

    [ADD]chart.js 추가와 렌더링을 위한 react-chart-js-2 설치

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -3,10 +3,12 @@ import Moisture from "./moisture";
 
 export default function DashBoard() {
   return (
-    <>
-      <p>대시보드입니당</p>
-      <Temperature />
-      <Moisture />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <p>대시보드입니당</p>
+        <Temperature />
+        <Moisture />
+      </Routes>
+    </BrowserRouter>
   );
 }
