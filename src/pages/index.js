852e3c8a26972ff4266e07d9c88c5367cbebeb06
김정연 <jy.keem2468@gmail.com>
2023-04-28 18:58:44 +0900

    [PROG]메인 화면 구현 중 배경 스타일 컴포넌트 작성중

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,44 @@
 import Link from "next/link";
+import styled from "@emotion/styled";
 
 export default function Home() {
   return (
     <main>
-      <header class="">
-        <div class="header_logo">
-          <p>안셍세요</p>
-        </div>
-        <div class="header_menu">
-          <p>메입니니</p>
-        </div>
-      </header>
       <div>
-        <video src="/images/backgroundVideo.mp4" loop autoPlay muted></video>
-        <div class="content">
-          <span>바질팜팜팜</span>
-        </div>
+        <MainPageVideoWrap>
+          <MainPageVideo src="/images/backgroundVideo.mp4" loop autoPlay muted>
+            <MainPageTextDiv>
+              <MainPageText>바질을 키우는 가장 스마트한 방법</MainPageText>
+            </MainPageTextDiv>
+          </MainPageVideo>
+        </MainPageVideoWrap>
       </div>
     </main>
   );
 }
+
+const MainPageVideo = styled.video`
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+`;
+
+const MainPageVideoWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to left, rgb(113, 113, 255), rgb(138, 255, 146));
+  mix-blend-mode: overlay;
+`;
+
+const MainPageTextDiv = styled.div`
+  z-index: -1;
+  font-color: rgba(#ffffff);
+`;
+
+const MainPageText = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: x-large;
+`;
