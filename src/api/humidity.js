ee23c93a357c7e2cbfdfaf8f9b650f2a23cf8cdf
수아 정 <mintolb12@gmail.com>
2023-05-02 00:22:48 +0900

    [DEL] 컨트롤러 페이지 삭제 [ADD] 컨트롤러 페이지 재구성, 이미지 추가

--- src/api/humidity.js
+++ src/api/humidity.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export function handleLedOnOff(value) {
+export function isLedOn(value) {
   //value 는 "on" 또는 "off" 로 해도 되고 true or false로 해도 될듯?
   axios
     .post("/api/actuators", { value })
--- src/pages/controller/index.jsx
+++ src/pages/controller/index.jsx
@@ -1,8 +1,80 @@
 import NavBar from "../../components/NavBar/NavBar";
-import Humidity from "../../components/Controller/Category/Humidity";
-import Light from "../../components/Controller/Category/Light";
 import styled from "@emotion/styled";
+import CardwithSquareImage from "../../components/Controller/Cards/CardwithSquareImage";
+import soilMoisture from "../../../public/images/soil_moisture.png";
+import airHumidity from "../../../public/images/air_humidity.png";
+import airTemperature from "../../../public/images/air_temperature.png";
+import illuminance from "../../../public/images/illuminance.png";
 
+//CardwithSquareImage에 prop 넘겨줄때 각각 넘겨주지 말고 객체를 넘기면 될것같은데->DB구성후에 생각하기로..
+const Controller = () => {
+  return (
+    <ContainerWrapper>
+      <NavBar></NavBar>
+      <Container>
+        <Panel left>
+          <CardwithSquareImage
+            image={airHumidity}
+            size={220}
+            subject={"airhumidity"}
+            subjectName={"대기 수분"}
+            measuredValue={40}
+            buttonText={"환풍기 켜기"}
+          ></CardwithSquareImage>
+          <CardwithSquareImage
+            image={airTemperature}
+            size={220}
+            subject={"temperature"}
+            subjectName={"대기 온도"}
+            measuredValue={22}
+            buttonText={"LED 켜기"}
+          ></CardwithSquareImage>
+        </Panel>
+        <Panel>
+          <CardwithSquareImage
+            image={soilMoisture}
+            size={220}
+            subject={"soilmoisture"}
+            subjectName={"토양 수분"}
+            measuredValue={23}
+            buttonText={"즉시 물주기"}
+          ></CardwithSquareImage>
+          <CardwithSquareImage
+            image={illuminance}
+            size={220}
+            subject={"illuminance"}
+            subjectName={"조도"}
+            measuredValue={67}
+            buttonText={"LED 켜기"}
+          ></CardwithSquareImage>
+        </Panel>
+      </Container>
+    </ContainerWrapper>
+  );
+};
+
+export default Controller;
+
+const ContainerWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 85%;
+  display: flex;
+  margin: 0 auto;
+`;
+
+const Panel = styled.div`
+  width: 50%;
+  margin-top: 8%;
+  padding: 2rem 2rem 2rem 2rem;
+  border-right: ${({ left }) => (left ? "solid 1px #8d8d8d" : "none")};
+`;
+
+/*
 const Controller = () => {
   return (
     <div>
@@ -15,6 +87,7 @@ const Controller = () => {
     </div>
   );
 };
+
 
 const Panels = styled.div`
   position: absolute;
@@ -29,3 +102,5 @@ const Space = styled.div`
   height: 80vh;
 `;
 export default Controller;
+
+*/
