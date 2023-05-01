92553776df4dd7ea0320bf72a4c2d1cc893134fc
김정연 <jy.keem2468@gmail.com>
2023-05-01 11:28:51 +0900

    [PROG]대시보드의 조도LED와 모터펌프 구현중

--- src/components/Dashboard/Motorpump.jsx
+++ src/components/Dashboard/Motorpump.jsx
@@ -1,4 +1,22 @@
-export dafault MotorPump(props) {
-    const motorpump = {}
-    return ();
+import { useState, useEffect } from 'react';
+
+export default function Motorpump(props) {
+  const [currentMotorpump, setCurrentMotorpump] = useState(null);
+  const [previousMotorpump, setPreviousMotorpump] = useState(null);
+  
+  useEffect(() => {
+    const currentMotorpumpValue = Motorpump(props);
+    setCurrentLight(currentMotorpumpValue);
+
+    if (previousMotorpump !== null) {
+      let difference = currentMotorpumpValue - previousMotorpump;
+      console.log(${difference});
+    }
+
+    setPreviousLight(currentMotorpumpValue);
+  }, [props]);
+
+  return (
+    ${difference}
+  );
 }
