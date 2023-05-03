4eb2bae1ff0739a6e6964f738fd70884ca95ad3f
김정연 <jy.keem2468@gmail.com>
2023-05-03 22:22:40 +0900

    [FEAT]토양 수분 센서 컴포넌트 구현

--- src/components/Dashboard/Moisture.jsx
+++ src/components/Dashboard/Moisture.jsx
@@ -1,35 +1,16 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useRecoilValue } from "recoil";
+import { dashboardDataAtom } from "@/store/Dashboard/atoms";
 
 // 토양 수분감지 센서
 export default function Moisture() {
-  const [currentMoisture, setCurrentMoisture] = useState(null);
-  const [previousMoisture, setPreviousMoisture] = useState(null);
-
-  const moistureArrowSign =
-    currentMoisture - previousMoisture > 0 ? "🔺" : "🔻";
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:3000/api/mockup/dashboard"
-        );
-        const data = res.data;
-        setCurrentMoisture(data.current);
-        setPreviousMoisture(data.previous);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchData();
-  }, [setCurrentMoisture, setPreviousMoisture]);
+  const dashboardData = useRecoilValue(dashboardDataAtom);
 
   return (
     <>
-      <h1>{currentMoisture}</h1>
+      <h1>{dashboardData[0]?.moisture}</h1>
       <h3>현재 토양 수분량</h3>
-      <p>{moistureArrowSign}</p>
-      <p>{Math.abs(currentMoisture - previousMoisture)}</p>
+      <p>{dashboardData[1] - dashboardData[0] > 0 ? "🔺" : "🔻"}</p>
+      <p>{Math.abs(dashboardData[1]?.moisture - dashboardData[0]?.moisture)}</p>
     </>
   );
 }
