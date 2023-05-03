cc35e12ac62c0a212808b21b50706adb2ee9d66b
김정연 <jy.keem2468@gmail.com>
2023-05-03 21:23:03 +0900

    [FEAT]NavBar 보완

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../store/Dashboard/atoms";
 import LightComponent from "@/components/Dashboard/Light";
 import Humidity from "@/components/Dashboard/Humidity";
+import NavBarTest from "@/components/NavBar/NavBarTest";
 
 export default function Dashboard() {
   const setDashboardData = useSetRecoilState(dashboardDataAtom);
@@ -44,7 +45,7 @@ export default function Dashboard() {
 
   return (
     <div>
-      <NavBar />
+      <NavBarTest />
       <DashboardDisplayFlex>
         <DashboardCommonAreaDiv>
           <Temp />
