055213d4fb1974709e98a0e4b7038fb5e690446d
김정연 <jy.keem2468@gmail.com>
2023-05-03 21:45:56 +0900

    [PROG]모터펌프, 식물 LED 컴포넌트 스타일 변경

--- src/components/Dashboard/Motorpump.jsx
+++ src/components/Dashboard/Motorpump.jsx
@@ -1,15 +1,30 @@
 import { useRecoilValue } from "recoil";
 import { actuatorDataAtom } from "../../store/Dashboard/atoms";
+import styled from "@emotion/styled";
 
 // 모터펌프
 export default function Motorpump() {
   const actuatorData = useRecoilValue(actuatorDataAtom);
 
   return (
-    <div>
+    <MotorpumpDiv>
       <h2>{actuatorData[1]?.pump}</h2>
-      <p>{actuatorData[1]?.pump ? "켜짐" : "꺼짐"}</p>
-      <p>모터펌프</p>
-    </div>
+      <p>{actuatorData[1]?.pump ? "On" : "Off"}</p>
+      <h3>모터펌프</h3>
+    </MotorpumpDiv>
   );
 }
+
+const MotorpumpDiv = styled.div`
+  align-items: center;
+  justify-content: center;
+
+  > h2 {
+    font-size: 6rem;
+  }
+
+  > p {
+    font-size: 1.2rem;
+    margin: 0.5rem;
+  }
+`;
