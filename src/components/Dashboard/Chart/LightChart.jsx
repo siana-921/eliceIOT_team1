6bd8237c8e3c384045bf08dba584ba028ab0a144
김정연 <jy.keem2468@gmail.com>
2023-05-05 00:48:53 +0900

    [ADD]조도차트 컴포넌트 새로 추가

---
+++ src/components/Dashboard/Chart/LightChart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+export default LightChart() {
+
+}
