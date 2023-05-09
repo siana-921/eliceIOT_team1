18cab09e9c31823992c98e31f490c76fc455808d
김정연 <jy.keem2468@gmail.com>
2023-05-09 16:56:40 +0900

    [PROG]회원가입에 제한사항 추가(email에 @필수, 비밀번호 글자수,특수문자,숫자,영문자 조건 추가)

--- src/pages/api/auth/signup.jsx
+++ src/pages/api/auth/signup.jsx
@@ -1,15 +1,55 @@
+import { hashPassword } from "../../../../lib/auth";
+
 async function handler(req, res) {
+  let prisma = new PrismaClient();
+
   if (req.method !== "POST") {
     return;
   }
 
   const data = req.body;
-  const { fullname, email, password, phone } = data;
+
+  const { id, fullname, email, password, phone } = data;
+  const passwordRegex =
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
+
+  if (
+    !id ||
+    !fullname ||
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    !phone ||
+    !passwordRegex.test(password.trim())
+  ) {
+    res.status(422).json({
+      message: "비밀번호는 8자 이상 16 이하로 입력하셔야 합니당근 ",
+      error: true,
+    });
+    return;
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { id: id },
+    select: {
+      email: true,
+      fullname: true,
+    },
+  });
+
+  if (existingUser) {
+    res.status(422).json({ message: "이미 로그아웃 되어써오!", error: true });
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
   const result = {
     id: 1,
     fullname: fullname,
     email: email,
     phone: phone,
+    password: hashedPassword,
   };
 
   if (result) {
