fbe0f88417de6dc14b4c1fe14fe47fa57c196ce4
김정연 <jy.keem2468@gmail.com>
2023-05-15 17:29:35 +0900

    [PROG]로고 이미지 추가 및 마이페이지 유저정보 마크업

--- src/components/MyPage/MyPageUser.jsx
+++ src/components/MyPage/MyPageUser.jsx
@@ -1,9 +1,48 @@
+import styled from "@emotion/styled";
+
 export default function MyPageUser() {
   return (
     <>
-      <main>
-        <h1>김정연님, 반가워요!</h1>
-      </main>
+      <MyPageUserMain>
+        <MyPageTitle>👋 김정연님, 반가워요!</MyPageTitle>
+        <MyPageDiv>
+          <MyPageInfoList>
+            <MyPageInfoItem>🪴 아이디 : testuser</MyPageInfoItem>
+            <MyPageInfoItem>✉️ 이메일 : 123@example.com</MyPageInfoItem>
+            <MyPageInfoItem>📱 휴대폰번호 : 010-1234-5678</MyPageInfoItem>
+          </MyPageInfoList>
+        </MyPageDiv>
+      </MyPageUserMain>
     </>
   );
 }
+
+const MyPageUserMain = styled.main`
+  width: 75%;
+  margin: 0 auto;
+
+  border-radius: 20px;
+  background-color: rgba(217, 217, 217, 0.3);
+
+  text-align: center;
+`;
+
+const MyPageTitle = styled.h1`
+  font-size: 50px;
+  font-weight: 300;
+
+  margin: 20px;
+`;
+
+const MyPageDiv = styled.div`
+  margin: 35px;
+`;
+
+const MyPageInfoList = styled.ul`
+  margin: 10px;
+`;
+
+const MyPageInfoItem = styled.li`
+  font-size: 23px;
+  margin: 5px;
+`;
--- src/pages/myPage/index.jsx
+++ src/pages/myPage/index.jsx
@@ -1,11 +1,28 @@
+import styled from "@emotion/styled";
+import Image from "next/image";
+
 import MyPageBailsList from "@/components/MyPage/MyPageBasilsList";
 import MyPageUser from "@/components/MyPage/MyPageUser";
 
 export default function MyPage() {
   return (
-    <>
+    <MyPageContainer>
+      <MyPageLogoImage src="/images/logo.png" alt="logo" layout="fill" />
       <MyPageUser />
       <MyPageBailsList />
-    </>
+    </MyPageContainer>
   );
 }
+
+const MyPageContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MyPageLogoImage = styled.img`
+  margin-top: 20px;
+
+  height: 80%;
+  width: 45%;
+`;
