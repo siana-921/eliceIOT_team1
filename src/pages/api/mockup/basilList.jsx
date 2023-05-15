96611772b99153257c7c7ad265e19c5ee7809c42
김정연 <jy.keem2468@gmail.com>
2023-05-16 00:49:21 +0900

    [PROG]mockup data 추가

--- src/pages/api/mockup/basilList.jsx
+++ src/pages/api/mockup/basilList.jsx
@@ -3,6 +3,23 @@
 export default function handler(req, res) {
   const data = {
     defaultDeviceId: "unit01",
+    devices: [
+      {
+        id: "unit01",
+        name: "Unit 1",
+        image: "path/to/device1.jpg",
+      },
+      {
+        id: "unit02",
+        name: "Unit 2",
+        image: "path/to/device1.jpg",
+      },
+      {
+        id: "unit02",
+        name: "Unit 2",
+        image: "path/to/device1.jpg",
+      },
+    ],
   };
 
   res.status(200).json(data);
